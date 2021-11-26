4c2f90d56787b12ca4e2752f20f29e14ccbcc931
Vasco <bgama90@hotmail.com>
2021-11-26 17:27:19 -0300

    subindo fixes

---
+++ next-crud/src/components/Botao.tsx
@@ -0,0 +1,20 @@
+interface BotaopProps {
+    children: any
+    className?: string
+    cor?: 'green' | 'blue' | 'gray'
+    onClick?: () => void
+
+}
+
+export default function Botao(props: BotaopProps) {
+    const cor = props.cor ?? 'gray'
+    return (
+        <button onClick={props.onClick} className={`
+            bg-gradient-to-r from-${cor}-400 to-${cor}-700
+            text-white px-4 py-2 rounded-md
+            ${props.className}
+        `}>
+            {props.children}
+        </button>
+    )
+}
--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
@@ -12,6 +15,19 @@ export default function Home() {
     new Cliente('Carlos', 23, '3'),
     new Cliente('Pedro', 54, '4'),
   ]
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Excluir ....${cliente.nome} `)
+  }
+
+  function salvarCliente(cliente: Cliente) {
+    console.log(cliente)
+  }
+
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -19,7 +35,36 @@ export default function Home() {
       text-white 
     `}>
       <Layout titulo="Cadastro Simples">
-        <Tabela clientes={clientes}></Tabela>
+        {visivel === 'tabela' ? (
+          <>
+            <div className="flex justify-end">
+              <Botao cor="green" className="mb-4">Novo Cliente</Botao>
+            </div>
+
+            <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado}
+              clienteExcluido={clienteExcluido}
+            />
+          </>
+        ) : (
+          <Formulario 
+          cliente={clientes[0]}
+          clienteMudou={salvarCliente}
+          cancelado={() => setVisivel('tabela')}
+          />
+        )}
+        <>
+          <div className="flex justify-end">
+            <Botao cor="green" className="mb-4" 
+            onClick={() => setVisivel('form')}>
+              Novo Cliente
+              </Botao>
+          </div>
+
+          <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado}
+            clienteExcluido={clienteExcluido}
+          />
+        </>
+
       </Layout>
     </div>
   )
